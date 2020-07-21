205ee8984c21b1345eeadd48074340df8c9ae9df
Andreas Frankenberger <andreasfrankenberger@gmail.com>
2020-07-21 13:37:51 +0200

    Publish package on release

--- generate-clients.sh
+++ generate-clients.sh
@@ -37,7 +37,8 @@ docker run --rm -v $(pwd):/local -u $(id -u ${USER}):$(id -g ${USER}) $IMAGE gen
       -g typescript-angular \
       -o /local/clients/typescript-angular ${COMMON_PARAMS} \
       --additional-properties=npmName=pricemonitor-client \
-      --additional-properties=ngVersion=10.0.0
+      --additional-properties=ngVersion=10.0.0 \
+      --additional-properties=npmRepository=https://npm.pkg.github.com
 
 # The generator inserts crappy typescript boundaries which only work for angualr 9. We need to update them.
 sed -i 's/\"typescript\": \">=3.6.0 <3.8.0\"/\"typescript\": \">=3.9.2 <4.0.0\"/g' clients/typescript-angular/package.json
